a60dbb22280cc71c34ee4501de60cc85a896e867
JupiterCoffeee <jvcfialho@gmail.com>
2024-03-12 15:59:11 +0000

    Configurated global styles with stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import { styled } from "@/styles";
+
+const Button = styled('button', {
+  backgroundColor: ''
+})
+
 export default function Home() {
   return (
-<h1>Hello World</h1>
+    <h1>Hello World</h1>
   );
 }
