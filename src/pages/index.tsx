6178be5941ad0418b07da7730709b3aebdf0076c
JupiterCoffeee <jvcfialho@gmail.com>
2024-03-14 17:36:10 +0000

    Connection with Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,74 @@
-import { styled } from "@/styles";
-import { HomeContainer, Product } from "@/styles/pages/home";
+import { GetStaticProps } from "next";
 import Image from "next/image";
 
-import shirt1 from "@/assets/Shirt/Shirt1.png"
-import shirt2 from "@/assets/Shirt/Shirt2.png"
-import shirt3 from "@/assets/Shirt/Shirt3.png"
+import { styled } from "@/styles";
+import { HomeContainer, Product } from "@/styles/pages/home";
 
-export default function Home() {
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+
+import { stripe } from "@/lib/stripe";
+import Stripe from "stripe";
+
+interface HomeProps {
+  products: {
+    id: string,
+    name: string,
+    imageUrl: string,
+    price: number
+  }[]
+}
+
+export default function Home({ products } : HomeProps) {
+  const [ sliderRef ] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
-        <Image src={shirt1} width={520} height={480} alt=""/>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map(product => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        )
+      })}
 
-      <Product>
-        <Image src={shirt2} width={520} height={480} alt=""/>
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product>
-        <Image src={shirt3} width={520} height={480} alt=""/>
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
     </HomeContainer>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  });
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price;
+    
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-br', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount / 100)
+    }
+  })
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 60 * 60 * 2,
+  }
 }
